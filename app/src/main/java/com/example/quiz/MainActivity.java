9f0460d4b4bc16902c3107f2cb24ea913d6229cc
‘Marek <marekkinasz@gmail.com>
2020-10-08 18:47:00 +0200

    Prepare model of quiz

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.quiz;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CheckBox q1a1 = findViewById(R.id.q1_a1);
-        q1a1.isChecked();
+
+        Button clearButton = findViewById(R.id.clearButton);
+        Button submitButton = findViewById(R.id.submitButton);
+
+        // disable buttons until switch state is changed
+        clearButton.setEnabled(false);
+        submitButton.setEnabled(false);
+
+        // connect switches with adequate buttons
+        Switch enableClearSwitch = findViewById(R.id.enableClear);
+        connectSwitchWithButtonState(enableClearSwitch, clearButton);
+
+        Switch enableSubmitSwitch = findViewById(R.id.enableSubmit);
+        connectSwitchWithButtonState(enableSubmitSwitch, submitButton);
+    }
+
+    private void connectSwitchWithButtonState(Switch theSwitch, Button button) {
+        theSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> button.setEnabled(isChecked));
     }
 }
